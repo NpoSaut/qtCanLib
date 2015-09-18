fd50e55e39fff857199fcb443f7c685d08a7b374
Nazemnykh Anton <nazemnykh.anton@gmail.com>
2015-09-18 10:05:18 +0500

    Убрана ненужная зависимость от stl

--- drivers/LinuxSocketCan/LinuxSocketCanSocketFactory.cpp
+++ drivers/LinuxSocketCan/LinuxSocketCanSocketFactory.cpp
@@ -11,7 +11,7 @@ LinuxSocketCanSocketFactory::LinuxSocketCanSocketFactory()
 
 LinuxSocketCanSocket *LinuxSocketCanSocketFactory::produce(QString interfaceName, int rxCapacity, int txCapacity)
 {
-    int number = SocketOpen(interfaceName.toStdString().c_str(), rxCapacity, txCapacity);
+    int number = SocketOpen(interfaceName.toAscii().data(), rxCapacity, txCapacity);
     return number > 0 ?
                 new LinuxSocketCanSocket (number, interfaceName, rxCapacity, txCapacity)
               : nullptr;
